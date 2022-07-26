d0822d9ec3deec0a7d84103410c0d4ffa951eccf
Isabella Souza <isabsantos92@gmail.com>
2022-07-26 18:37:31 -0300

    feat: initialize schema

--- app/gateway/db/postgres/account/create.go
+++ app/gateway/db/postgres/account/create.go
@@ -8,5 +8,14 @@ import (
 
 func (r Repository) Create(ctx context.Context, acc account.Account) (account.Account, error) {
 	const operation = `Repository.Account.Create`
+
+	const statment string = `
+		INSERT into
+			accounts (document, secret, name, balance)
+		values 
+			($1, $2, $3, $4)
+		returning 
+			id, external_id, created_at, updated_at
+	`
 	return account.Account{}, nil
 }
